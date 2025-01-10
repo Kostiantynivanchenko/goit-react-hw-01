30f755e6f7dfc3bc275961d10512f166609b0881
kostiantyn_ivanchenko <kokos2702@gmail.com>
2025-01-10 01:28:40 +0100

    final-hopefully

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import App from "./App";
-import "./styles.css";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./styles.css";
+import App from "./App";
 
-createRoot.render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
